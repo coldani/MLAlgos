64ade98b178adb350a4603fd29216a6a17eb0973
Daniele <46798787+coldani@users.noreply.github.com>
2019-10-06 17:47:02 +0100

    Add files via upload

---
+++ various.py
@@ -0,0 +1,117 @@
+import numpy as np
+import numpy.linalg as la
+
+class pca:
+    '''
+    computes the principal components of a matrix
+    --> assumes that variables are in the columns, observations in the rows
+    '''
+
+    def __init__(self, data):
+        self.data = data
+    
+    def varcov(self, data):
+        '''
+        returns the varcov matric of the data
+        '''
+        varcov = np.cov(data, rowvar=False)
+        return varcov
+
+    def eig_val_vec(self, data):
+        '''
+        returns the eigenvalues and eigenvectors of the varcov matric of the data
+        '''
+        varcov = self.varcov(data)
+        eig_val, eig_vec = la.eig(varcov)
+        return (eig_val, eig_vec)
+    
+    def pc(self, data, num=1):
+        '''
+        returns the feature vector with the 'num'-greatest pc, as well as the % of variance explained
+        '''
+        eig_val, eig_vec = self.eig_val_vec(data)
+        max_eig_val = np.sort(eig_val)[-num:][::-1]
+        arg_max_eig_val = np.argsort(eig_val)[-num:][::-1]
+        max_eig_vec = eig_vec[:,arg_max_eig_val]
+
+        pc = np.empty(shape=(len(eig_val),num))
+        pc = np.matmul(data, max_eig_vec)
+        var_explained = np.sum(max_eig_val) / np.sum(eig_val)
+        return (pc, var_explained)
+
+def feature_scaling(x, method='stand'):
+    '''
+    x: np ndarray, with shape (num_obs, num_var)
+    method: string. Valid methods are 'stand', 'norm', 'scale'
+            'stand': standardization of the variables, with 0 mean and uniot variance
+            'norm': normalization of the variables, with 0 mean and values between [-1,1]
+            'scale': normalization of the variables, with 0.5 mean and values between [0,1]
+    returns: the scaled version of the array according to the chosen method
+    '''
+    valid_methods = ('stand', 'norm', 'scale')
+    assert method in valid_methods, 'invalid scaling method'
+
+    means = np.mean(x, axis=0)
+    maxs = np.max(x, axis=0)
+    mins = np.min(x, axis=0)
+    ranges = maxs - mins
+    std = np.std(x, axis=0)
+    
+    if method == 'stand':
+        scaled = (x - means)/std
+    elif method == 'norm':
+        scaled = 2*( (x - means)/ranges )
+    elif method == 'scale':
+        scaled = (x - mins)/ranges
+
+    return scaled
+
+def split_dataset(x, y, cross_validation_size=0.3, test_size=0.0, add_test=False, shuffle=True):
+    '''
+    x: a np.ndarray, with shape (num_obs, num_var)
+    y: a np.array
+    cross_validation_size: the size of the cross-validation set (fraction of all data)
+    test_size: the size of the test set (fraction of all data). Used only if add_test = True
+    add_test: boolean. If true, splits the dataset into 3 parts (training, cross-validation and test sets)
+    shuffle: boolean, if True randomly shuffles the data
+
+    returns: a training set, a cross-validation set and, if add_test = True, a test set
+    '''
+    assert len(y) == x.shape[0], 'x and y have different sizes!'
+    if not(add_test):
+        test_size = 0.0
+    y = y.reshape((len(y),1))
+    m = len(y)
+
+    data = np.append(y,x,axis=1)
+    if shuffle:
+        np.random.shuffle(data)
+
+    training_size = 1 - cross_validation_size - test_size
+    training_index = int(m*training_size)
+    cross_validation_index = training_index + int(m*cross_validation_size)
+    
+    if add_test:
+        training_set = data[:training_index,:]
+        cross_validation_set = data[training_index:cross_validation_index,:]
+        test_set = data[cross_validation_index:,:]
+        return (training_set, cross_validation_set, test_set)
+
+    else:
+        training_set = data[:training_index,:]
+        cross_validation_set = data[training_index:,:]
+        return (training_set, cross_validation_set)
+
+def add_intercept(x):
+    '''
+    x: a np.ndarray, with observations on the rows and variables on the columns. Can be 3-dim array (e.g. RGB images)
+    returns: the x array with the intercept (a constant value equal to 1) added on the first column 
+    '''
+    if x.ndim == 3:
+        intercept = np.ones((x.shape[0], 1, x.shape[2]))
+    else:
+        intercept = np.ones((x.shape[0], 1))
+           
+    x = np.append(intercept, x, axis=1)
+
+    return x
